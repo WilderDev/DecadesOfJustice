bacabe10be0db9d059b05bed52d5f8c929268ecf
wnorfleet15 <wnorfleet15@outlook.com>
2023-11-28 10:09:28 -0600

    added routing to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
+    path: '', redirectTo: '/Home', pathMatch: 'full'}, //Landing Page
 
+  { path: "", component:,children:[{path:"", component:}]}, //Creating Capsules
+  { path: "", component:,children:[{path:"", component:}]}, //Current Capsules
+  { path: "", component:,children:[{path:"", component:}]}, //Opening Capsules
+  { path: "", component:,children:[{path:"", component:}]}, //Authentication
 ];
 
 @NgModule({
