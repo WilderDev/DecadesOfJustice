8782c85077f700cc63ca319218ac5ee8a96df25a
gitmissv <vmlambert@semo.edu>
2023-12-14 19:02:00 -0600

    Updates
    ***SPELLING UPDATE***
    removed 'Home' (navbar)
    created 'under construction' alerts

--- src/app/core/landing-page/landing-page.component.ts
+++ src/app/core/landing-page/landing-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class LandingPageComponent {
 
+  ndrConstruct() {
+    return alert('Currently Under Construction. Check back later.')
+  }
+
 }
